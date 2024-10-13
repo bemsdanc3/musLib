3a337544ae2f367b3e75a6db91e5acd053f28580
bemsdanc3 <vedeneevigor94@yandex.ru>
2024-10-13 23:30:17 +0300

    create ca base

--- Backend/cmd/main.go
+++ Backend/cmd/main.go
@@ -1,5 +1,7 @@
 package main
 
-func main() {
+import "Backend/internal/app"
 
+func main() {
+	app.StartServer()
 }
--- Backend/pkg/db/db.go
+++ Backend/pkg/db/db.go
@@ -1,5 +1,37 @@
 package db
 
-func InitDb() {
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func InitDb() (*sql.DB, error) {
+	err := godotenv.Load("configs/.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	name := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, pass, host, port, name)
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	log.Println("Connected to database")
+	return db, nil
 
 }
